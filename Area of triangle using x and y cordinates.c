4c8a40127d8f9324848ceb599f9fff873f3a72dd
KHMANJUNATH <KHMANJUNATH447@GMAIL.COM>
2019-10-22 16:36:18 +0100

    Documentation
    Documentation was made by adding comments

--- Area of triangle using x and y cordinates.c
+++ Area of triangle using x and y cordinates.c
@@ -1,8 +1,9 @@
+//This is a Program used to calculate the area of a triangle using the x and y cordinates of the three sidde of a triangle
 #include<stdio.h>
 #include<math.h>
 main()
 {
-int x1,x2,x3,y1,y2,y3;
+int x1,x2,x3,y1,y2,y3;// point a(x1,y1),(x2,y2),(x3,y3)
 float area;
 printf("enter (x,y) vertices of triangle \n");
 scanf("%d%d%d%d%d%d",&x1,&y1,&x2,&y2,&x3,&y3);
